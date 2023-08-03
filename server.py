3d9c21769340e937133201510e5a6c9df0555c55
Stupid Bald Baby <139823344+stupidbaldbaby@users.noreply.github.com>
2023-08-03 01:33:32 -0400

    a lot of changes like uh kicking and banning and banning might have been in the last build uhhhhh idk

--- server.py
+++ server.py
@@ -11,10 +11,11 @@ import bson
 
 basedir = os.getcwd()
 
-# dict that contains all connected clients and their client ids
-connected = {
-	"0": "Server"
-}
+# dict that contains all connected clients and their authed users
+connected = {}
+
+# dict that contains the timestamp of a clients last request
+lc = {}
 
 # credits to Meower for this idea, all error codes and easy nicknames to refer to them
 errors = {
@@ -32,7 +33,8 @@ errors = {
 	"exists": "601 - Exists",
 	"unauthed": "602 - Unauthorized",
 	"authed": "603 - Authorized",
-	"state": "604 - Invalid State"
+	"state": "604 - Invalid State",
+	"ratelimit": "605 - Ratelimited"
 }
 
 if(os.path.isfile("./mongo.uri")):
@@ -95,7 +97,7 @@ class	db: # database operations
 		pid = str(uuid.uuid4())
 		datatosend = {
 			"_id": pid,
-			"username": connected[str(id)],
+			"username": id,
 			"content": content,
 			"timestamp": ts,
 			"likes": 0,
@@ -148,23 +150,38 @@ class	db: # database operations
 			return False
 	
 def loginclientwithid(client, username): # binds client id and username
-	cltemp = client["id"]
-	connected[str(cltemp)] = username
-	print("User " + username + " is now bound to connected ID " + str(cltemp))
+	client["username"] = username
+	connected[username] = client
+	print("User " + username + " is now bound to connected ID " + str(client["id"]))
 	ws.sendClient(client, errors["bound"])
-	print(str(connected))
+	print(str(connected) + " //")
 
 def new_client(client, server):
 	ws.sendClient(client, str(client))
 	
 def left_client(client, server):
-	cltemp = client["id"]
-	if str(cltemp) in connected:
-		del connected[str(cltemp)]
-		print(str(connected))
+	if("username" in client):
+		authin = client["username"]
+		if str(authin) in connected:
+			del connected[str(authin)]
+			print(str(connected))
+	if(str(client["id"]) in lc):
+		del lc[str(client["id"])]
 	
 def on_msg(client, server, message):
 	os.chdir(basedir)
+	if(str(client["id"]) in lc):
+		lca = lc[str(client["id"])]
+		lcab = time.time() - lca
+		if(lcab>=0.3):
+			lc[str(client["id"])] = time.time()
+		else:
+			ws.sendClient(client, errors["ratelimit"])
+			print("ID " + str(client["id"]) + " (ratelimited): " + str(message))
+			return None
+	else:
+		lc[str(client["id"])] = time.time()
+		print("added a client to the ratelimit // " + str(lc))
 	print("ID " + str(client["id"]) + ": " + str(message))
 	continuing = True
 	try:
@@ -181,10 +198,13 @@ def on_msg(client, server, message):
 		elif(r["ask"]=="signup"):
 			if("username" in r):
 				if("password" in r):
-					if(db.insertUser(r["username"], r["password"])):
-						loginclientwithid(client, r["username"])
+					if(not "username" in client):
+						if(db.insertUser(r["username"], r["password"])):
+							loginclientwithid(client, r["username"])
+						else:
+							ws.sendClient(client, errors["signup_error"])
 					else:
-						ws.sendClient(client, errors["signup_error"])
+						ws.sendClient(client, errors["authed"])
 				else:
 					ws.sendClient(client, errors["malformed"])
 			else:
@@ -192,32 +212,34 @@ def on_msg(client, server, message):
 		elif(r["ask"]=="login"):
 			if("username" in r):
 				if("password" in r):
-					auth = db.authUser(r["username"], r["password"])
-					if(auth=="done"):
-						loginclientwithid(client, r["username"])
-					elif(auth=="invalid"):
-						ws.sendClient(client, errors["invalid_pass"])	
-					elif(auth=="banned"):
-						ws.sendClient(client, errors["banned"])	
-					elif(auth=="notmade"):
-						ws.sendClient(client, errors["invalid_user"])
+					if(not "username" in client):
+						auth = db.authUser(r["username"], r["password"])
+						if(auth=="done"):
+							loginclientwithid(client, r["username"])
+						elif(auth=="invalid"):
+							ws.sendClient(client, errors["invalid_pass"])	
+						elif(auth=="banned"):
+							ws.sendClient(client, errors["banned"])	
+						elif(auth=="notmade"):
+							ws.sendClient(client, errors["invalid_user"])
+						else:
+							ws.sendClient(client, errors["login_error"])
 					else:
-						ws.sendClient(client, errors["login_error"])
+						ws.sendClient(client, errors["authed"])
 				else:
 					ws.sendClient(client, errors["malformed"])
 			else:
 				ws.sendClient(client, errors["malformed"])
 		elif(r["ask"]=="post"):
 			if("msg" in r):
-				cltemp = str(client["id"])
-				if(cltemp in connected):
-					a = db.getUser(connected[cltemp])
+				if("username" in client):
+					a = db.getUser(client["username"])
 					if(a==None):
 						ws.sendClient(client, errors["corrupted"])
 					elif(a["banned"]):
 						ws.sendClient(client, errors["banned"])
 					else:
-						stuff = db.insertPost(cltemp, r["msg"])
+						stuff = db.insertPost(client["username"], r["msg"])
 						if(stuff=="done"):
 							ws.sendClient(client, errors["ok"])
 						elif(stuff=="fail"):
@@ -231,28 +253,49 @@ def on_msg(client, server, message):
 		elif(r["ask"]=="ping"):
 			ws.sendClient(client, "Pong!")
 		elif(r["ask"]=="get_posts"):
-			ws.sendClient(client, str(db.getPosts()))
+			if("username" in client):
+				ws.sendClient(client, str(db.getPosts()))
+			else:
+				ws.sendClient(client, errors["unauthed"])
 		elif(r["ask"]=="get_user"):
 			if("username" in r):
-				ws.sendClient(client, str(db.getUser(r["username"])))
+				if("username" in client):
+					ws.sendClient(client, str(db.getUser(r["username"])))
+				else:
+					ws.sendClient(client, errors["unauthed"])
 			else:
 				ws.sendClient(client, errors["malformed"])
 		elif(r["ask"]=="ban"):
 			if("username" in r):
-				cltemp = str(client["id"])
-				if(cltemp in connected):
-					a = db.getUser(connected[cltemp])
+				if("username" in client):
+					a = db.getUser(client["username"])
 					if(a["state"]!=0):
-						ab = db.getUncleanedUser(r["username"])
+						ab = db.getUser(r["username"])
 						if(ab==None):
 							ws.sendClient(client, errors["invalid_user"])
 						else:
 							ab["banned"] = True
 							abc = db.changeUser(r["username"], "banned", True)
 							if(abc):
+								abcd = connected[r["username"]]
+								abcd["handler"].send_close(1000, bytes('', encoding='utf-8'))
 								ws.sendClient(client, errors["ok"])
 							else:
 								ws.sendClient(client, errors["idk"])
+					else:
+						ws.sendClient(client, errors["state"])
+				else:
+					ws.sendClient(client, errors["unauthed"])
+			else:
+				ws.sendClient(client, errors["malformed"])
+		elif(r["ask"]=="kick"):
+			if("username" in r):
+				if("username" in client):
+					a = db.getUser(client["username"])
+					if(a["state"]!=0):
+						abcd = connected[r["username"]]
+						abcd["handler"].send_close(1000, bytes('', encoding='utf-8'))
+						ws.sendClient(client, errors["ok"])
 					else:
 						ws.sendClient(client, errors["state"])
 				else:
